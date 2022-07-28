976c5de0aa818c1b372ed01862474305588f29d1
RonalldoTeixeira <ronalldo.tb@gmail.com>
2022-07-28 20:39:24 -0300

    respostas exer-10

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+
+const tx = ("No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes: O primeiro deve possuir apenas **números**, ne estes números não devem seguir nenhuma ordem específica; O segundo array deve possuir apenas **strings**; O terceiro array deve possuir **números**, **strings** e **booleanos**. Imprima cada um dos arrays acima")
+console.log(tx);
+const pri = [5]
+const seg = ["caderno"]
+const ter =[9, "lua", false]
+
+console.log(pri);
+console.log(seg);
+console.log(ter);
